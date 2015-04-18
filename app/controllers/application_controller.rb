474a37c53ad1df9176a02e247f575c20031e85be
Jeremy Gagon <jergagon1@gmail.com>
2015-04-18 16:31:06 -0700

    Delete anon_reports

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   protected
-  def after_sign_in_path_for(resource)
-    # p resource
-  end
+  # def after_sign_in_path_for(resource)
+  #   # p resource
+  # end
 
 end
